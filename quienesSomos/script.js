44c4fce3f3bf778217ad10607eb834cc3a2fe41e
Dani <daniDJmax1@gmail.com>
2023-03-24 23:48:04 +0000

    Quienes somos Responsive + paquetes

--- quienesSomos/script.js
+++ quienesSomos/script.js
@@ -1,22 +1,17 @@
-fetch('pruebacargar.json')
-  .then(response => response.json())
-  .then(data => {
-    let quienesSomos = data.quienesSomos;
-    let quienesSomosDiv = document.getElementById('quienesSomos');
-    
-    quienesSomos.forEach(producto => {
-      let contenedorDescripcionDiv = document.createElement('div');
-      contenedorDescripcionDiv.idName = 'contenedorDescripcion';
-      
-      let descripcion = document.createElement('p');
-      descripcion.idName = 'descripcion';
-      descripcion.textContent = quienesSomos.descripcion;
-      contenedorDescripcionDiv.appendChild(descripcion);
-      
-      contenedorDescripcionDiv.appendChild(contenedorDescripcionDiv);
-      
-      
-      
-    });
-  })
-  .catch(error => console.error(error));
+fetch('http://localhost:3000/pruebaCargar.json')
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+   /*.then(response => response.json())
+   .then(data => console.log(data))
+   .then(data => {
+      const contenedorDescripcion = document.querySelector('#contenedorDescripcion');
+      data.quienesSomos.forEach(elemento => {
+         const p = document.createElement('p');
+         p.textContent = `${elemento.descripcion}`;
+         contenedorDescripcion.appendChild(p);
+      });
+   })
+   .catch(error => {
+      console.error('Error al obtener los datos del archivo JSON:', error);
+   });*/
