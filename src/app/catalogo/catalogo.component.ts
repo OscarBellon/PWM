e059008e0c9c53bb780088df8b2b1e75808a10e2
Oscar <oscar.bellon101@alu.ulpgc.es>
2023-05-01 13:12:50 +0100

    preparacion para la carga dinamica

--- src/app/catalogo/catalogo.component.ts
+++ src/app/catalogo/catalogo.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+interface product {
+
+}
 @Component({
   selector: 'app-catalogo',
   templateUrl: './catalogo.component.html',
   styleUrls: ['./catalogo.component.scss']
 })
-export class CatalogoComponent {
 
+
+export class CatalogoComponent {
+  productList: product[] =[];
 }
