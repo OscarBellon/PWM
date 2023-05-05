f9e6cf76b10a48d9990d1d6988742bca927d73b0
daniDJmax1 <daniDJmax1@gmail.com>
2023-05-05 01:48:19 +0100

    Login Working

--- src/app/acceder/acceder.component.ts
+++ src/app/acceder/acceder.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { from } from 'rxjs';
+import UserService from '../services/user.services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-acceder',
   templateUrl: './acceder.component.html',
   styleUrls: ['./acceder.component.scss']
 })
-export class AccederComponent {
+export class AccederComponent implements OnInit{
+  formLogin: FormGroup;
 
+  constructor(
+    private userService: UserService,
+    private fb: FormBuilder,
+    private router: Router
+  ){
+    this.formLogin = this.fb.group({
+      email: new FormControl('',[Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required,Validators.minLength(6)])
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(){
+    this.userService.login(this.formLogin.value)
+      .then(response => {
+        console.log(response);
+        alert("Acceso completado");
+        this.router.navigate(['/home']);
+      })
+      .catch(error => console.log(error));
+    console.log(this.formLogin.value);
+
+  }
 }
